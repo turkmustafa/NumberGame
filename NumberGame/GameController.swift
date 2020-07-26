fcee62b8aa70cb0fb47bff15f58f9f6664af02f5
Mustafa,Turk <mustafaturk43@gmail.com>
2020-07-26 21:19:53 +0300

    Project Setup

---
+++ NumberGame/GameController.swift
@@ -0,0 +1,18 @@
+//
+//  GameController.swift
+//  NumberGame
+//
+//  Created by Mustafa Türk on 26.07.2020.
+//  Copyright © 2020 Mustafa Türk. All rights reserved.
+//
+
+import UIKit
+
+class GameController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
