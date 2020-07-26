ed05e0be27cd4ca702c01ab7681150544eea0052
Mustafa,Turk <mustafaturk43@gmail.com>
2020-07-26 20:56:58 +0300

    Initial Commit

---
+++ NumberGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NumberGame
+//
+//  Created by Mustafa Türk on 26.07.2020.
+//  Copyright © 2020 Mustafa Türk. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
